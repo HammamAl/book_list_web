e069a468d0ab33ddb832fef18ddcfd2056733490
HammamAl <alfarisihammam@gmail.com>
2025-04-18 06:45:23 +0700

    perbarui repo

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("book_db", "root", "", {
-  host: "localhost",
+c;
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   dialect: "mysql",
-  logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  },
 });
 
 export default sequelize;
